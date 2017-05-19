1e4ddd8e44bed0b178d368770cbe02bd12cacf92
xiaoliushifu <872140945@qq.com>
2017-05-19 10:09:37 +0800

    这是学习yii框架以来，第一次提交yii的代码，而且接下来要学习一个强大的gii模块功能，据说可以生成很多代码文件，比如控制器，模型，视图，以及模型里curd代码，我觉得比laravel的migrate强大多了呢，不多说了。先保留一下吧

---
+++ controllers/CountryController.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\controllers;
+
+#导入基准父控制器
+use yii\web\Controller;
+//导入分页组件（小部件）
+use yii\data\Pagination;
+//导入模型类
+use app\models\Country;
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        
+        $query = Country::find();
+        
+        $pagination = new Pagination([
+            'defaultPageSize'=>5,
+            'totalCount'=>$query->count(),
+        ]);
+        
+        $countries = $query->orderBy('name')->offset($pagination->offset)->limit($pagination->limit)->all();
+        
+        return $this->render('index',[
+            'countries'=>$countries,
+            'pagination'=>$pagination,
+            
+        ]);
+    }
+}
