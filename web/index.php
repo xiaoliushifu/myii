7231085259dfb142a653cef45c2b07df8b414f6e
xiaoliushifu <872140945@qq.com>
2017-06-01 09:42:41 +0800

    整理一下日志组件的学习，暂停一下，因为我发现再走下去或许会很慢，因为又到了学无可用，所以走不长的模式了。改去研究路由了

--- web/index.php
+++ web/index.php
@@ -8,5 +8,6 @@ require(__DIR__ . '/../vendor/autoload.php');
 //注册yii框架自己的自动加载机制，并首次出现Yii全局对象，还把容器对象填充到Yii的属性$container
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+//开始自己的配置，还可以有更多的配置文件
 $config = require(__DIR__ . '/../config/web.php');
 (new yii\web\Application($config))->run();
