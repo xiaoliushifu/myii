a990d4740f34f834662e12a243b6a2704122b7d6
xiaoliushifu <872140945@qq.com>
2017-07-26 15:17:48 +0800

    Event::trigger()方法的注释与源码阅读 事件对象是所有事件处理者被调用执行时传递的共享参数， 任何一个事件处理者对事件对象的改变，都将影响后一个事件处理者对事件对象的使用

--- vendor/yiisoft/yii2/base/Event.php
+++ vendor/yiisoft/yii2/base/Event.php
@@ -198,44 +198,58 @@ class Event extends Object
     }
 
     /**
+     * 触发一个类级别的事件
      * Triggers a class-level event.
+	 * 这个方法将会触发该类的该事件的所有事件处理者方法的调用，父类的也会调用
      * This method will cause invocation of event handlers that are attached to the named event
      * for the specified class and all its parent classes.
      * @param string|object $class the object or the fully qualified class name specifying the class-level event.
      * @param string $name the event name.
+	 * 事件对象，如果不传，则使用Event对象
+	 * 事件对象很重要，是所有事件处理者在调用时所共享的对象，前一个事件处理者对$event的操作，会影响到后续的事件
+	 * 处理者。
+	 * 事件对象保留了事件发生时的一些有用信息：事件名name，事件触发者sender，是否handled等
+	 * 当然，也可以在触发时自定义其他有用的信息
      * @param Event $event the event parameter. If not set, a default [[Event]] object will be created.
      */
     public static function trigger($class, $name, $event = null)
     {
+		//根本没这个事件，那还触发个啥，直接退出!
         if (empty(self::$_events[$name])) {
             return;
         }
+		//第三个参数为空，则实例化调用者，注意是static，而不是Event。
         if ($event === null) {
             $event = new static;
         }
+		//定义一个标记
         $event->handled = false;
-        $event->name = $name;
+        $event->name = $name; //事件名，保存到事件对象的name属性
 
         if (is_object($class)) {
             if ($event->sender === null) {
+				//把当前对象传递给事件对象的sender属性，这样在事件处理者中就能访问知道是哪个对象触发的了。
                 $event->sender = $class;
             }
             $class = get_class($class);
         } else {
             $class = ltrim($class, '\\');
         }
-
+		//当前类，父类，接口一并拿来
         $classes = array_merge(
             [$class],
             class_parents($class, true),
             class_implements($class, true)
         );
-
+		//遍历调用$name事件，$class类的事件处理者队列里的成员。
         foreach ($classes as $class) {
             if (!empty(self::$_events[$name][$class])) {
                 foreach (self::$_events[$name][$class] as $handler) {
+					//每个事件处理者的参数都在绑定时确定，而非事件触发时确定，在事件处理者内部通过event::data访问
                     $event->data = $handler[1];
                     call_user_func($handler[0], $event);
+					//对象都是引用传参，故在事件处理者内部转了一圈回到这里，会影响它的handled属性。
+					//只要某个事件处理者把handled置为true,那么后续的事件处理者就都不再调用执行了
                     if ($event->handled) {
                         return;
                     }
