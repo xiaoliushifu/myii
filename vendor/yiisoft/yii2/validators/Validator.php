7687dda9fc4b93c2afa04771ffa0ee67b99b446a
xiaoliushifu <872140945@qq.com>
2017-08-09 00:29:49 +0800

    验证基类yii\validator\validator的开端注释 尚未读完

--- vendor/yiisoft/yii2/validators/Validator.php
+++ vendor/yiisoft/yii2/validators/Validator.php
@@ -11,13 +11,14 @@ use Yii;
 use yii\base\Component;
 use yii\base\NotSupportedException;
 
-/**
+/**Validator是所有验证器类的基类
  * Validator is the base class for all validators.
- *
+ *子类应该覆盖validateValue或validateAttribute方法以提供实际的验证逻辑。
  * Child classes should override the [[validateValue()]] and/or [[validateAttribute()]] methods to provide the actual
+ * 子类也可以覆盖clientValidateAttribute()方法以支持客户端验证
  * logic of performing data validation. Child classes may also override [[clientValidateAttribute()]]
  * to provide client-side validation support.
- *
+ *Validator声明了一套内置的验证器，这些用别名列在下面：
  * Validator declares a set of [[builtInValidators|built-in validators]] which can
  * be referenced using short names. They are listed as follows:
  *
@@ -45,7 +46,7 @@ use yii\base\NotSupportedException;
  * - `unique`: [[UniqueValidator]]
  * - `url`: [[UrlValidator]]
  * - `ip`: [[IpValidator]]
- *
+ * 这些验证器的详细使用，看guide article
  * For more details and usage information on Validator, see the [guide article on validators](guide:input-validation).
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
@@ -97,65 +98,76 @@ class Validator extends Component
         'ip' => 'yii\validators\IpValidator',
     ];
     /**
+	 * 数组或者字符串，存储当前验证器要验证的属性，在初始化验证器时赋值
      * @var array|string attributes to be validated by this validator. For multiple attributes,
+	 * 多个属性时，请使用数组，单个属性时，使用字符串或者数组都行
      * please specify them as an array; for single attribute, you may use either a string or an array.
      */
     public $attributes = [];
-    /**
+    /**字符串类型，用户自定义的错误信息，格式如下，有一些可以填充的地方
      * @var string the user-defined error message. It may contain the following placeholders which
      * will be replaced accordingly by the validator:
-     *
+     *属性的标签
      * - `{attribute}`: the label of the attribute being validated
+	 * 属性的值
      * - `{value}`: the value of the attribute being validated
-     *
+     *注意，有些验证器可能会引入新的特性，来封装错误信息，当指定的验证条件实现时
      * Note that some validators may introduce other properties for error messages used when specific
      * validation conditions are not met. Please refer to individual class API documentation for details
      * about these properties. By convention, this property represents the primary error message
      * used when the most important validation condition is not met.
      */
     public $message;
-    /**
+    /**数组或者字符串，当前验证器应用的场景。多场景请指定为数组，单个场景用字符串或数组都行
      * @var array|string scenarios that the validator can be applied to. For multiple scenarios,
      * please specify them as an array; for single scenario, you may use either a string or an array.
      */
     public $on = [];
-    /**
+    /**数组或字符串，当前验证器应该排除在这些个场景。多场景请指定为数组，单个场景用字符串或数组都行
      * @var array|string scenarios that the validator should not be applied to. For multiple scenarios,
      * please specify them as an array; for single scenario, you may use either a string or an array.
      */
     public $except = [];
-    /**
+    /**boolean值，是否某个属性在被其他验证器已经验证为失败的情况下还会被当前验证器验证，
      * @var bool whether this validation rule should be skipped if the attribute being validated
+	 * 默认是true,意味着前面的验证器把某个属性验证失败时，轮到当前验证器时就跳过。
      * already has some validation error according to some previous rules. Defaults to true.
      */
     public $skipOnError = true;
-    /**
+    /**boolean值，当属性值是null或者emtpy时，是否跳过它而不进行验证。
      * @var bool whether this validation rule should be skipped if the attribute value
      * is null or an empty string.
+	 *默认也是跳过
      */
     public $skipOnEmpty = true;
-    /**
+    /**boolean,是否开启客户端验证
      * @var bool whether to enable client-side validation for this validator.
+	 * 实际的客户端验证代码，是由clientValidateAttribute()方法返回的。
      * The actual client-side validation is done via the JavaScript code returned
+	 * 如果方法里并没有写客户端代码，那么即使这个属性设置为true，客户端验证还是开展不下去。
      * by [[clientValidateAttribute()]]. If that method returns null, even if this property
      * is true, no client-side validation will be done by this validator.
      */
     public $enableClientValidation = true;
-    /**
+    /**回调函数，用来替换默认的isEmpty()方法。
      * @var callable a PHP callable that replaces the default implementation of [[isEmpty()]].
+	 * 如果该属性为空，那就用默认的isEmpty()方法判空。
      * If not set, [[isEmpty()]] will be used to check if a value is empty. The signature
+	 * 函数只包含一个参数，函数的返回值是boolean,表名当前的属性是否是empty
      * of the callable should be `function ($value)` which returns a boolean indicating
      * whether the value is empty.
      */
     public $isEmpty;
-    /**
+    /**这个属性的值是个回调函数，返回boolean,来决定当前的验证器是否应用于关联的属性（条件验证）
      * @var callable a PHP callable whose return value determines whether this validator should be applied.
+	 * 函数的签名是function($model,$attribute),返回值是boolean.
      * The signature of the callable should be `function ($model, $attribute)`, where `$model` and `$attribute`
      * refer to the model and the attribute currently being validated. The callable should return a boolean value.
-     *
+     * 这个属性主要提供是否支持客户端验证（条件验证）的逻辑
      * This property is mainly provided to support conditional validation on the server-side.
+	 * 如果这个属性为空（也就是说无需条件验证），则当前验证器总是验证涉及的属性
      * If this property is not set, this validator will be always applied on the server-side.
-     *
+     * 回调函数的例子看下面，当country选择了USA的时候，验证开启！
      * The following example will enable the validator only when the country currently selected is USA:
      *
      * ```php
@@ -167,15 +179,17 @@ class Validator extends Component
      * @see whenClient
      */
     public $when;
-    /**
+    /**字符串，一个JS函数，返回值决定了是否进行客户端验证（条件验证）
      * @var string a JavaScript function name whose return value determines whether this validator should be applied
+	 * 函数的签名是function(attribute,value)。attribute就是当前被验证的属性，value就是当前属性的值
      * on the client-side. The signature of the function should be `function (attribute, value)`, where
      * `attribute` is an object describing the attribute being validated (see [[clientValidateAttribute()]])
      * and `value` the current value of the attribute.
-     *
+     *这个属性其实提供了客户端的条件验证
      * This property is mainly provided to support conditional validation on the client-side.
+	 *如果属性为空，则总是在客户端验证
      * If this property is not set, this validator will be always applied on the client-side.
-     *
+     *下面的例子开启了客户端条件验证的支持。当country选择了USA的时候
      * The following example will enable the validator only when the country currently selected is USA:
      *
      * ```javascript
@@ -216,6 +230,7 @@ class Validator extends Component
             $params['class'] = __NAMESPACE__ . '\InlineValidator';
             $params['method'] = $type;
         } else {
+			//是数组的形式，属于内置验证器
             if (isset(static::$builtInValidators[$type])) {
                 $type = static::$builtInValidators[$type];
             }
