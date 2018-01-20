09fd88c37ff35c98d1ca83672fcf8fc96409f3e0
liushifu <872140945@qq.com>
2018-01-20 17:52:37 +0800

    第一部分是：asset解决前端资源的版本问题 第二部分是：今天的邮件报警，我测试一下我的yunzongnet邮箱，竟然也发不出去，但是也不显示为啥，用zend的debug才看到，真的是密码错了。 最终换了喜云的邮箱可以发送出去，这才更换no-reply的邮箱试试报密码错误，才知道no-reply的密码真的是错误了。

--- assets/MyAsset.php
+++ assets/MyAsset.php
@@ -14,9 +14,17 @@ use yii\web\AssetBundle;
  */
 class MyAsset extends AssetBundle
 {
+	//源资源，是和php代码放到一块的地方，需要发布到web可访问的目录才能用
+	//发布资源，即已发布过的资源，一般经源资源的发布就变成为已发布资源，一般发布到web可访问的目录
+	//外部资源，就是外网的，不是在本地应用的。
+
+
     //basePath是发布资源的属性，指明在Web服务器允许对外访问的哪个目录,@webroot就是D:/wamp64/www/basic/web
+	//另外，还有一个好处，就是assetManager配置appendTimestamp为true时，还会根据js，css文件的修改
+	//自动加上最后修改的时间戳，这样就可以控制静态资源文件的版本信息了。无需我们手动在url后写?a=xxx了
     public $basePath = '@webroot';
-    //baseUrl就是相对域名的外部路径，@web是/basic/web,也就是www.xx.com/basic/web
+    //baseUrl就是相对域名的外部路径，@web是/basic/web,也就是www.xx.com/basic/web,
+	//将来配合$js最终可以找到这个js文件，也就是说它会拼接在$js之前
     public $baseUrl = '@web';
     
     //
