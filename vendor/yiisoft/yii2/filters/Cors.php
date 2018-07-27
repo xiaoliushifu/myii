7952d0c9157da796672c0bca103a06c807c49f28
liumingwei <liumingwei@xiyun.com.cn>
2018-07-27 16:34:39 +0800

    刚才没有提交上，Cors的源码注解在这哩

--- vendor/yiisoft/yii2/filters/Cors.php
+++ vendor/yiisoft/yii2/filters/Cors.php
@@ -13,14 +13,15 @@ use yii\web\Request;
 use yii\web\Response;
 
 /**
-*
-* Cors过滤器就是为了实现跨源资源共享的
  * Cors filter implements [Cross Origin Resource Sharing](http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
- *请一定仔细阅读了解CORS做了什么和不做什么。CORS并不会增加API的安全性，它是允许开发者分配访问资源的权限给第三方的代码（一般是ajax从外域调用服务端）
+ *
  * Make sure to read carefully what CORS does and does not. CORS do not secure your API,
  * but allow the developer to grant access to third party code (ajax calls from external domain).
-
- * 具体使用CORS就是配置它作为一个行为给控制器或模块，如下：
+ *
+ * 读完Cors过滤器，好像有点糊涂，糊涂的是：并没有做验证和什么，仅仅是用协商好的头部手段
+ * 达到协商的目的，比如告诉客户端允许的自定义header,什么请求方法，什么来源是否允许等；仅仅如此；
+ * 客户端得到这个成功后，应该就知道如何再次请求服务端了；至于服务端允许不允许还是服务端说了算。
+ * 那这个Cors的用处在那儿呢？默认是ajax跨域限制，通过首先发送options请求，且据此的响应，后续可以跨域了，达到跨域的目的？？？
  * You may use CORS filter by attaching it as a behavior to a controller or module, like the following,
  *
  * ```php
@@ -33,7 +34,7 @@ use yii\web\Response;
  *     ];
  * }
  * ```
- *也可以配置严格的参数限制 详见MDN cors 信息
+ *
  * The CORS filter can be specialized to restrict parameters, like this,
  * [MDN CORS Information](https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS)
  *
@@ -61,7 +62,7 @@ use yii\web\Response;
  *     ];
  * }
  * ```
- *  详情如何给控制器添加该过滤器，看指南[Guide on REST controllers](guide:rest-controllers#cors).
+ *
  * For more information on how to add the CORS filter to a controller, see
  * the [Guide on REST controllers](guide:rest-controllers#cors).
  *
@@ -84,9 +85,11 @@ class Cors extends ActionFilter
     public $actions = [];
     /**
      * @var array Basic headers handled for the CORS requests.
+     * 这个数组是配置接收客户端的请求header部分字段，也就是说，服务端从http客户端请求的这几个header字段里作为判断协商的关键；
+     * 也可以自定义，但是目前默认的如下五个够用了
      */
     public $cors = [
-        'Origin' => ['*'],
+        'Origin' => ['*'],//Origin字段指明客户端的来源信息，客户端是哪个域名或者ip的，*代表服务端的限制规则是任意域名，ip
         'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
         'Access-Control-Request-Headers' => ['*'],
         'Access-Control-Allow-Credentials' => null,
@@ -97,23 +100,29 @@ class Cors extends ActionFilter
 
     /**
      * @inheritdoc
+     * 这是过滤器的入口
      */
     public function beforeAction($action)
     {
+        //取得http客户端对象
         $this->request = $this->request ?: Yii::$app->getRequest();
+        //取得http响应对象
         $this->response = $this->response ?: Yii::$app->getResponse();
 
+        //这个暂时没有用到
         $this->overrideDefaultSettings($action);
-
+        //从$_SERVER里找到需要的header
         $requestCorsHeaders = $this->extractHeaders();
+        //处理这些header，响应给客户端对应的header
         $responseCorsHeaders = $this->prepareHeaders($requestCorsHeaders);
+        //把响应header添加到响应对象中
         $this->addCorsHeaders($this->response, $responseCorsHeaders);
 
         return true;
     }
 
     /**
-     * Override settings for specific action
+     * Override settings for specific action.
      * @param \yii\base\Action $action the action settings to override
      */
     public function overrideDefaultSettings($action)
@@ -130,7 +139,7 @@ class Cors extends ActionFilter
     }
 
     /**
-     * Extract CORS headers from the request
+     * Extract CORS headers from the request.
      * @return array CORS headers to handle
      */
     public function extractHeaders()
@@ -138,17 +147,21 @@ class Cors extends ActionFilter
         $headers = [];
         $requestHeaders = array_keys($this->cors);
         foreach ($requestHeaders as $headerField) {
+            //这一步很重要，需要把本地配置的cors加上HTTP_,因为php的习惯问题
+            //$_SERVER收到的http请求header,都会加上HTTP_前缀。
+            //这样外部实际的http请求header和php的$_SERVER是不同的，多了HTTP_前缀
             $serverField = $this->headerizeToPhp($headerField);
             $headerData = isset($_SERVER[$serverField]) ? $_SERVER[$serverField] : null;
             if ($headerData !== null) {
                 $headers[$headerField] = $headerData;
             }
         }
+
         return $headers;
     }
 
     /**
-     * For each CORS headers create the specific response
+     * For each CORS headers create the specific response.
      * @param array $requestHeaders CORS headers we have detected
      * @return array CORS headers ready to be sent
      */
@@ -162,6 +175,7 @@ class Cors extends ActionFilter
             }
         }
 
+        //这一步是咋回事，为啥单独对Headers做处理呢？
         $this->prepareAllowHeaders('Headers', $requestHeaders, $responseHeaders);
 
         if (isset($requestHeaders['Access-Control-Request-Method'])) {
@@ -172,7 +186,7 @@ class Cors extends ActionFilter
             $responseHeaders['Access-Control-Allow-Credentials'] = $this->cors['Access-Control-Allow-Credentials'] ? 'true' : 'false';
         }
 
-        if (isset($this->cors['Access-Control-Max-Age']) && Yii::$app->getRequest()->getIsOptions()) {
+        if (isset($this->cors['Access-Control-Max-Age']) && $this->request->getIsOptions()) {
             $responseHeaders['Access-Control-Max-Age'] = $this->cors['Access-Control-Max-Age'];
         }
 
@@ -184,7 +198,7 @@ class Cors extends ActionFilter
     }
 
     /**
-     * Handle classic CORS request to avoid duplicate code
+     * Handle classic CORS request to avoid duplicate code.
      * @param string $type the kind of headers we would handle
      * @param array $requestHeaders CORS headers request by client
      * @param array $responseHeaders CORS response headers sent to the client
@@ -199,7 +213,9 @@ class Cors extends ActionFilter
         if (in_array('*', $this->cors[$requestHeaderField])) {
             $responseHeaders[$responseHeaderField] = $this->headerize($requestHeaders[$requestHeaderField]);
         } else {
-            $requestedData = preg_split("/[\\s,]+/", $requestHeaders[$requestHeaderField], -1, PREG_SPLIT_NO_EMPTY);
+            //把这些按照逗号拆分
+            $requestedData = preg_split('/[\\s,]+/', $requestHeaders[$requestHeaderField], -1, PREG_SPLIT_NO_EMPTY);
+            //计算交集，用strcasecmp做回调函数处理
             $acceptedData = array_uintersect($requestedData, $this->cors[$requestHeaderField], 'strcasecmp');
             if (!empty($acceptedData)) {
                 $responseHeaders[$responseHeaderField] = implode(', ', $acceptedData);
@@ -208,7 +224,7 @@ class Cors extends ActionFilter
     }
 
     /**
-     * Adds the CORS headers to the response
+     * Adds the CORS headers to the response.
      * @param Response $response
      * @param array $headers CORS headers which have been computed
      */
@@ -223,15 +239,17 @@ class Cors extends ActionFilter
     }
 
     /**
-     * Convert any string (including php headers with HTTP prefix) to header format like :
-     *  * X-PINGOTHER -> X-Pingother
-     *  * X_PINGOTHER -> X-Pingother
+     * Convert any string (including php headers with HTTP prefix) to header format.
+     *
+     * Example:
+     *  - X-PINGOTHER -> X-Pingother
+     *  - X_PINGOTHER -> X-Pingother
      * @param string $string string to convert
      * @return string the result in "header" format
      */
     protected function headerize($string)
     {
-        $headers = preg_split("/[\\s,]+/", $string, -1, PREG_SPLIT_NO_EMPTY);
+        $headers = preg_split('/[\\s,]+/', $string, -1, PREG_SPLIT_NO_EMPTY);
         $headers = array_map(function ($element) {
             return str_replace(' ', '-', ucwords(strtolower(str_replace(['_', '-'], [' ', ' '], $element))));
         }, $headers);
@@ -239,9 +257,11 @@ class Cors extends ActionFilter
     }
 
     /**
-     * Convert any string (including php headers with HTTP prefix) to header format like :
-     *  * X-Pingother -> HTTP_X_PINGOTHER
-     *  * X PINGOTHER -> HTTP_X_PINGOTHER
+     * Convert any string (including php headers with HTTP prefix) to header format.
+     *
+     * Example:
+     *  - X-Pingother -> HTTP_X_PINGOTHER
+     *  - X PINGOTHER -> HTTP_X_PINGOTHER
      * @param string $string string to convert
      * @return string the result in "php $_SERVER header" format
      */
