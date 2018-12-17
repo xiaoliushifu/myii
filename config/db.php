98f4b9d26c5bbcc685c946d37f0fc53cbbab7f47
masterliu <872140945@qq.com>
2018-12-17 22:56:34 +0800

    delete db.php
    对已经提交到远程仓库到文件，需要先删除然后本地pull；最后在把它添加到gitignore里

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.100;dbname=test',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
